29417b6f1dcd252f639de569607d9e5029bd9ef3
hlhq1 <hlhq1@163.com>
2017-04-20 15:30:20 +0800

    Angular2 exmaple Tour of Heroes

---
+++ app/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html',
+  styleUrls:[ 'dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeros();
+  }
+
+  getHeros(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+    // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+}
